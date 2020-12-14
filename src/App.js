894d65b0a78be5b47d0d5088c6683a2accc0c623
rishi <prabhak8@uni.coventry.ac.uk>
2020-12-14 12:51:50 +0000

    updated proxy

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import ZooplaProperties from "./components/zoopla/zooplaProperties";
  */
 const App = () => {
   const [ user, setUser ] = useState(null)
-  const api = 'api/v1/login'
-  const initialPropertyUrl = "api/v1/properties?page=1&limit=5"
+  const api = '/api/v1/login' || '/proxy/api/v1/login'
+  const initialPropertyUrl = "/api/v1/properties?page=1&limit=5" || '/proxy/api/v1/properties?page=1&limit=5'
   const [ propertiesUrl, setPropertiesUrl] = useState(initialPropertyUrl)
   const [ errorData, setErrorData ] = useState({})
   const [ showErrModal, setShowErrModal ] = useState(false)
